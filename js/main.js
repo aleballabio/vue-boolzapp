0a036042d5100a874fcec4f0f08f9aa0016f05dd
aleballabio <aleballabio05@gmail.com>
2022-03-16 12:38:25 +0100

    fixed text and messages

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ const app = new Vue({
     el: '#root',
     data: {
         searchingProfile: "",
+        activeChat: 0,
         activeProfile:
         {
             nome: "Alessandro",
             image: "avatar_io.jpg",
             textMessage: "",
-            activeChat: 0,
+            time: "",
         },
 
         chats: [
@@ -15,11 +16,13 @@ const app = new Vue({
                 nome: "Mario",
                 image: "avatar_1.jpg",
                 lastMessage: "",
+                lastAccess: "",
                 messages: [
                     {
-                        text: "",
-                        time: "",
-                    }
+                        text: "Miao Miao",
+                        time: "12:25",
+                        received: true,
+                    },
                 ]
             },
 
@@ -27,42 +30,98 @@ const app = new Vue({
                 nome: "Genoveffa",
                 image: "avatar_2.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "Miao Miao",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
 
             {
                 nome: "Michelina B.",
                 image: "avatar_3.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "Miao Miao",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
 
             {
                 nome: "Benedetta",
                 image: "avatar_4.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "Miao Miao",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
 
             {
                 nome: "Susan",
                 image: "avatar_5.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
 
             {
                 nome: "Carletto T.",
                 image: "avatar_6.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
 
             {
                 nome: "Portina",
                 image: "avatar_7.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
 
             {
                 nome: "Yang J.",
                 image: "avatar_8.jpg",
                 lastMessage: "",
+                lastAccess: "",
+                messages: [
+                    {
+                        text: "",
+                        time: "12:25",
+                        received: true,
+                    },
+                ]
             },
         ]
     },
@@ -71,6 +130,17 @@ const app = new Vue({
         changeCurrentChat(number) {
             this.activeChat = number;
             console.log(this.activeChat);
+        },
+
+        sendMessage(currentChat) {
+            this.chats[currentChat].messages.push({
+                text: this.activeProfile.textMessage,
+                date: "",
+                received: false,
+            });
+            this.activeProfile.textMessage = "";
+            console.log(currentChat);
+            console.log(this.chats[currentChat].messages);
         },
     }
 });
